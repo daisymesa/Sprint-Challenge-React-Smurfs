db258b6e19a0b30f147acb81e2fff27c7f2f95f4
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-05-24 13:16:48 -0400

    Added styles to Navigation, updated fonts

--- village/src/components/Navigation.js
+++ village/src/components/Navigation.js
@@ -7,14 +7,18 @@ const Navigation = () => {
     return (
         <div>
             <div className="navigation">
-                <img src="https://vignette.wikia.nocookie.net/smurfs/images/5/5e/Smurfs_villge_harmony.jpg/revision/latest/scale-to-width-down/670?cb=20150812140625" />
+                <img src="https://vignette.wikia.nocookie.net/smurfs/images/5/5e/Smurfs_villge_harmony.jpg/revision/latest/scale-to-width-down/670?cb=20150812140625" alt="the smurfs village" />
 
                 <div className="nav-links">
-                    <div>
-                        <NavLink exact to="/">Home</NavLink>
+                    <div className="nav-link">
+                        <NavLink exact to="/">
+                            <p>Village Home</p>
+                        </NavLink>
                     </div>
-                    <div>
-                        <NavLink to="/smurf-form">Add New Smurfs!</NavLink>
+                    <div className="nav-link">
+                        <NavLink to="/smurf-form">
+                            <p>Add New Smurfs!</p>
+                        </NavLink>
                     </div>
                 </div>
             </div>
